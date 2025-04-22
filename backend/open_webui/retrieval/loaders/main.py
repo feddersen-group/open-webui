cf11d9a8b8966c0f12652e4bd67b6b96f873fc76
Alexander Grimm <43762102+almajo@users.noreply.github.com>
2025-04-22 12:48:37 +0200

    (fix): add max limit for gemini extraction (#5)
    Only use Gemini PDF extraction for PDFs with less than 20 pages. Otherwise, use the built in PdfPDF reader.
    
    Co-authored-by: Alexander Grimm <agrimm@ailio.de>

--- backend/open_webui/retrieval/loaders/main.py
+++ backend/open_webui/retrieval/loaders/main.py
@@ -20,6 +20,7 @@ from langchain_community.document_loaders import (
     UnstructuredXMLLoader,
 )
 from langchain_core.documents import Document
+from pypdf import PdfReader
 
 from open_webui.retrieval.loaders.external_document import ExternalDocumentLoader
 
@@ -28,6 +29,7 @@ from open_webui.retrieval.loaders.datalab_marker import DatalabMarkerLoader
 
 
 from open_webui.env import SRC_LOG_LEVELS, GLOBAL_LOG_LEVEL
+from feddersen.config import PDF_EXTRACTION_GEMINI_PAGE_LIMIT
 from feddersen.loaders.gemini import GeminiLoader
 
 logging.basicConfig(stream=sys.stdout, level=GLOBAL_LOG_LEVEL)
@@ -365,7 +367,13 @@ class Loader:
             )
         else:
             if file_ext == "pdf":
-                if self.kwargs.get("LITELLM_BASE_URL") and self.kwargs.get("LITELLM_API_KEY"):
+                # This is not yet rendering all pages but rather the format
+                num_pages = PdfReader(file_path).get_num_pages()
+                if (
+                    self.kwargs.get("LITELLM_BASE_URL")
+                    and self.kwargs.get("LITELLM_API_KEY")
+                    and num_pages <= PDF_EXTRACTION_GEMINI_PAGE_LIMIT
+                ):
                     loader = GeminiLoader(
                         base_url=self.kwargs.get("LITELLM_BASE_URL"),
                         api_key=self.kwargs.get("LITELLM_API_KEY"),
