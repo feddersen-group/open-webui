9ec1753594bd718e4817700e7809ce4428c9cae0
Jack <jack.taylor@feddersen.group>
2025-10-21 13:54:39 +0200

    (fix): format backend

--- backend/open_webui/routers/retrieval.py
+++ backend/open_webui/routers/retrieval.py
@@ -1507,7 +1507,8 @@ def process_file(
                 try:
                     # /files/{file_id}/data/content/update
                     VECTOR_DB_CLIENT.delete_collection(
-                        collection_name=f"file-{file.id}")
+                        collection_name=f"file-{file.id}"
+                    )
                 except:
                     pass
 
@@ -1543,7 +1544,7 @@ def process_file(
                     ]
                 else:
                     docs = []
-                    
+
                 text_content = file.data.get("content", "")
             else:
                 # Process the file and save the content
